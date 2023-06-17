cd70f4244e79e131f9dcdcc5081a72d44b15467b
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-06-17 16:37:54 +0900

    revise chap5-2.py

--- chap5/chap5-2.py
+++ chap5/chap5-2.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from pprint import pprint
 
 OPEN_WEATHER_MAP_API_KEY = os.environ.get('OPEN_WEATHER_MAP_API_KEY')
 # print(OPEN_WEATHER_MAP_API_KEY)
@@ -11,4 +12,5 @@ url = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&lang
 url = url.format(city="Tokyo,JP", key=OPEN_WEATHER_MAP_API_KEY)
 
 json_data = requests.get(url).json()
-print(json_data)
+# print(json_data)
+pprint(json_data)
